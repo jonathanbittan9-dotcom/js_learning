ea6a05a9cc631e3ef485a621b3ace136eb96edb3
Steven Hawking <jonathan.bittan9@gmail.com>
2026-03-25 22:43:43 +0200

    varibles and basics

---
+++ lesson1-basics/script.js
@@ -0,0 +1,11 @@
+//console.log("hello");
+//console.log("i like pizza");
+
+//window.alert("This is an alert");
+//window.alert("I like pizza");
+
+document.getElementById("myh1").textContent = "hello" ;
+document.getElementById("myp").textContent = "i like pizza"
+
+// This is a comment
+
---
+++ lesson2-varibales/script.js
@@ -0,0 +1,52 @@
+//let x =123;
+
+//console.log(x)
+// let age=25;
+
+//console.log(age)
+
+// let price = 10.99;
+
+//console.log(price)
+
+// let gpa = 2.1;
+
+// let firstname = "human";
+// let favoritefood = "sushi";
+// let email = "human.com"
+
+// console.log(typeof firstname);
+// console.log(firstname);
+// console.log(`your first name is ${firstname}`);
+// console.log(`your favorite food is ${favoritefood}`);
+// console.log(`your email is ${email}`);
+
+// let online = true;
+// let forsale = false;
+// let isStudent = true;
+
+// console.log(typeof online)
+
+// console.log(`bro is online on discord: ${online}`)
+// console.log(`is this car for sale: ${forsale}`)
+// console.log(`Enrolled: ${isStudent}`)
+
+
+
+
+
+// console.log(`you are ${age} years old`);
+// console.log(`the price is ${price}`)
+// console.log(`your gpa is: ${gpa}`)
+// console.log(typeof age)
+// console.log(typeof price)
+// console.log(typeof gpa)
+
+
+
+
+
+let fullname = "human";
+let age = 25;
+let student = false;
+document.getElementById( )
