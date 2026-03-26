83663893caa35d8aa0c5e86c081c5449ddeb5a38
Steven Hawking <jonathan.bittan9@gmail.com>
2026-03-26 17:34:10 +0200

    lesson3-part one

---
+++ lesson3-operatos/script.js
@@ -0,0 +1,20 @@
+// let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students **2
+// students = students % 2
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /=2;
+// students **= 2;
+// students %= 2;
+
+// students ++;
+// students --;
+
+// console.log(students)
